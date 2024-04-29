3230a387d505be96d4477d29d1c04984a9046446
melanie-galea <90864246+melanie-galea@users.noreply.github.com>
2024-04-29 17:16:41 +0200

    Update kenlm.py

--- kenlm/kenlm.py
+++ kenlm/kenlm.py
@@ -14,21 +14,8 @@ class Token:
 
 class TokenRanker(abc.ABC):
     best_score_selector = max
-    """
-    The function to use when choosing the best score.
-    This can be overridden by inheritors & is mostly applicable with the default implementation of :py:meth:`~TokenRanker.filter_best`.
-    """
 
     def filter_best(self, alternatives: list[Token]) -> list[Token]:
-        """
-        Args:
-            alternatives: All the alternative tokens to consider.
-
-        Returns:
-            The filtered list of alternatives which have the best score.
-            If multiple alternatives are given,
-            this means that all the alternatives are considered equally good by the :class:`TokenRanker`.
-        """
         scores = self.score([alternative.text for alternative in alternatives])
         return np.asarray(alternatives)[np.asarray(scores) == self.best_score_selector(scores)]
 
@@ -44,30 +31,12 @@ class TokenRanker(abc.ABC):
         pass
 
 
-class RandomRanker(TokenRanker):
-    """
-    Ranks tokens by sorting them alphabetically.
-    """
-
-    def filter_best(self, alternatives: list[Token]) -> list[Token]:
-        return [sorted(alternatives, key=lambda token: token.text)[0]]
-
-    def score(self, alternatives: list[str]) -> list[float]:
-        pass
-
-
 class SubTokensCountRanker(TokenRanker):
-    """
-    Ranks tokens by the number of sub-tokens given by the underlying tokenizer.
-    """
+    #ranks the tokens by the number of sub-tokens given by the underlying tokenizer.
 
     best_score_selector = min
 
     def __init__(self, name: str):
-        """
-        Args:
-            name: The name or path to the tokenizer to use from the ``transformers`` library.
-        """
         self.tokenizer = AutoTokenizer.from_pretrained(name)
 
     def score(self, alternatives: list[str]) -> list[float]:
@@ -75,48 +44,36 @@ class SubTokensCountRanker(TokenRanker):
 
 
 class WordModelScoreRanker(TokenRanker):
-    """
-    Ranks tokens by the word n-gram language model score.
-    """
+    #ranks the tokens by the word n-gram language model score
 
-    def __init__(self, name: str):
-        """
-        Args:
-            name: The path to the model to use through the ``kenlm`` library.
-        """
-        self.model = kenlm.Model(name)
+    def __init__(self):
+        self.model = model
 
     def score(self, alternatives: list[str]) -> list[float]:
         return [self.model.score(token) for token in alternatives]
 
 
 class CharacterModelScoreRanker(TokenRanker):
-    """
-    Ranks tokens by the character n-gram language model score.
-    """
+    #ranks the tokens by the charater n-gram language model score
 
-    def __init__(self, name: str):
-        """
-        Args:
-            name: The path to the model to use through the ``kenlm`` library.
-        """
-        self.model = kenlm.Model(name)
+    def __init__(self):
+        self.model = model
 
     def score(self, alternatives: list[str]) -> list[float]:
         return [self.model.score(' '.join(token)) for token in alternatives]
 
 def fix_spelling(sentence, ranker):
-    # Tokenize the sentence
+    # tokenize  sentence
     tokens = sentence.strip().split(' ')
     print("this is tokens: ", tokens[1])
 
-    # Rank the tokens
+    # rank the tokens
     token_objects = [Token(token) for token in tokens]
     best_tokens = ranker.filter_best(token_objects)
     for token in best_tokens:
       print("here: ", token.text)
 
-    # Replace misspelled tokens with best-ranked tokens
+    # replace misspelled tokens with best-ranked tokens
     corrected_sentence = " ".join(token.text for token in best_tokens)
 
     return corrected_sentence
